9df0aef4ec08b5a729a0d36424f567dc48bd3cfe
ritesh2k <19952553+ritesh2k@users.noreply.github.com>
2022-09-16 01:54:19 +0530

    add bind polyfill implementation

---
+++ Problems/BindPolyfill.js
@@ -0,0 +1,25 @@
+const obj = {first:'ritesh',
+            last:'kumar'}
+
+
+Function.prototype.myBind = function (...params) {
+    // setting the this ref to a variable to form a closure
+    const obj = this;
+    console.log(params)
+    
+        return function(...returnedFunctionParamList){
+        // using apply to handle mutiple args;
+        // combine args from bind polyfill and the returned function call args
+            
+        obj.apply(params[0],[...params.splice(1), ...returnedFunctionParamList])
+    }
+}
+
+
+function printName(age,town) {
+    console.log(`${this.first} ${this.last} ${age} ${town}`)
+}
+
+const print =printName.myBind(obj,27)
+
+print('Auckland')
