ef3ee437ac6962d918a902d1275e1cdbafcef9a8
ritesh <19952553+ritesh2k@users.noreply.github.com>
2023-09-20 20:56:05 +0530

    re-arrange few files

--- Problems/recursion/NumPalindrome.js
+++ Problems/recursion/NumPalindrome.js
@@ -1,19 +1,16 @@
-var isPalindrome = function(x) {
-
+//check if a number is palindrome without using string methods
+var isPalindrome = function (x) {
     let originalNum = x;
-    if (x < 0)
-        return false;
+    if (x < 0) return false;
     let reverseNum = 0;
     let multiplier = 10;
     while (originalNum > 0) {
-        reverseNum = (reverseNum*multiplier)+((originalNum % 10));
+        reverseNum = reverseNum * multiplier + (originalNum % 10);
         //multiplier *= 10;
-        originalNum = Math.floor(originalNum / 10)
-
+        originalNum = Math.floor(originalNum / 10);
     }
 
-    return reverseNum === x
-
+    return reverseNum === x;
 };
 
-isPalindrome(120021)
+isPalindrome(120021);
