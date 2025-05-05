36e3e09c3688e0b810b92738b064163650732a0f
ritesh2k <riiteshKumaar@gmail.com>
2025-05-05 23:52:56 +0530

    optimize bubble and selection sort algos

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,23 +1,18 @@
-const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1]
+const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1];
 
-
-const bubbleSort = (arr) => {
-    const sortedArray = [...arr]
+const bubbleSort = arr => {
     for (let index = 0; index < arr.length; index++) {
-
-        for (let compareIndex = 0; compareIndex < sortedArray.length - index - 1; compareIndex++) {
-            if (sortedArray[compareIndex] > sortedArray[compareIndex + 1]) {
-                let temp = sortedArray[compareIndex]
-                sortedArray[compareIndex] = sortedArray[compareIndex + 1]
-                sortedArray[compareIndex + 1] = temp
+        for (let compareIndex = 0; compareIndex < arr.length - index - 1; compareIndex++) {
+            // we decrease the camparision window by 1 as larger element will be bubbled to the end
+            if (arr[compareIndex] > arr[compareIndex + 1]) {
+                //if current element is larger than next, swap them
+                let temp = arr[compareIndex];
+                arr[compareIndex] = arr[compareIndex + 1];
+                arr[compareIndex + 1] = temp;
             }
         }
-
     }
-    return sortedArray
+    return arr;
+};
 
-}
-
-
-console.log(`Sorted Array`, bubbleSort(originalArray))
-
+console.log(`Sorted Array`, bubbleSort(originalArray));
--- Sorting/SelectionSort.js
+++ Sorting/SelectionSort.js
@@ -1,17 +1,20 @@
+const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1];
 
-const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1]
-
-const insertionSort = (arr) => {
+function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
+        let indexOfmin = i;
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            if (arr[indexOfmin] > arr[j]) {
+                indexOfmin = j;
             }
         }
-
+        if (i !== indexOfmin) {
+            // if current index is not equal to index of min, swap the values
+            let lesser = arr[indexOfmin];
+            arr[indexOfmin] = arr[i];
+            arr[i] = lesser;
+        }
     }
-    console.log(`originalArray`, originalArray)
+    return arr;
 }
-insertionSort(originalArray)
+console.log(selectionSort(originalArray));
