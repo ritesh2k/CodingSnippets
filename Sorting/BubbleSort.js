43f015d793d5b76fcbf87c16e7815332cfa72f25
Ritesh Kumar <19952553+ritesh2k@users.noreply.github.com>
2025-06-25 18:07:22 +0530

    Update BubbleSort.js
    add additional way of using do while loop

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -16,3 +16,22 @@ const bubbleSort = arr => {
 };
 
 console.log(`Sorted Array`, bubbleSort(originalArray));
+
+// instead of using two for loop using one for and one  do while loop
+// function bubbleSort(arr) {
+//   let swap;
+//   do {
+//     swap = false;
+//     for (let i = 0; i < arr.length - 1 ; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//       let  temp = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = temp;
+//         swap = true;
+//       }
+//     }
+//   } while (swap);
+//   return arr;
+// }
+
+// console.log(bubbleSort([8, -2, 6, 0, 5, -22, 33,1,4,4]))
