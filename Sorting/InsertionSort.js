d021f89050d7c661170b844cda2223ac1173d0af
ritesh2k <riiteshKumaar@gmail.com>
2025-06-26 00:50:22 +0530

    add slightly different way to do insertion sort

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,21 +1,32 @@
-const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1]
+const originalArray = [200, 12, 354, 123, 11, 3, 23, 3534, 12313, 112, -1];
 
-const insertionSort = (arr) => {
-    const sortedArray = [...arr]
+const insertionSort = arr => {
     for (let index = 0; index < arr.length; index++) {
-        const currentElement = sortedArray[index];
-        let sortedIndex = index
-        while (currentElement < sortedArray[sortedIndex - 1] && sortedIndex > 0) {
-            sortedArray[sortedIndex] = sortedArray[sortedIndex - 1]
-            sortedIndex--
+        const currentElement = arr[index];
+        let sortedIndex = index;
+        while (currentElement < arr[sortedIndex - 1] && sortedIndex > 0) {
+            arr[sortedIndex] = arr[sortedIndex - 1];
+            sortedIndex--;
         }
-        sortedArray[sortedIndex] = currentElement
-
+        arr[sortedIndex] = currentElement;
     }
-    return sortedArray
+    return arr;
+};
+const insertionSortOtherApproach = arr => {
+    for (let i = 1; i < arr.length; i++) {
+        //we start the loop at 1st index
+        let numberToBeInserted = arr[i];
+        let sortedIndex = i - 1; // assume that first element is sorted and then insert other element before or after it
+        while (sortedIndex >= 0 && arr[sortedIndex] > numberToBeInserted) {
+            arr[sortedIndex + 1] = arr[sortedIndex]; // shift the greater element of the sorted array to the right,
+            //  to make space for the element to be inserted
+            sortedIndex--;
+        }
+        arr[sortedIndex + 1] = numberToBeInserted; // we do j+1 because when condition fails j is pointing to the element lesser that current element
+        // so we insert the current element to the right of j
+    }
+    return arr;
+};
 
-}
-
-
-console.log(`insertionSort(originalArray)`, insertionSort(originalArray))
-
+console.log(`insertionSort(originalArray)`, insertionSort(originalArray));
+console.log(`insertionSortOtherApproach`, insertionSortOtherApproach(originalArray));
